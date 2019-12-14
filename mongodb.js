fa1771ca467b5bdefd73d80dc9d5ed750c33604a
Jean Carlos De Castro <jeancarlosdecastro@Jeans-MacBook-Pro.local>
2019-12-14 13:58:37 -0500

    completed CRUD training

--- mongodb.js
+++ mongodb.js
@@ -1,3 +1,5 @@
+// learning CRUD
+
 const { MongoClient, ObjectID } = require('mongodb')
 
 const connectionURL = 'mongodb://127.0.0.1:27017'
@@ -11,28 +13,76 @@ MongoClient.connect(connectionURL, { useNewUrlParser: true ,  useUnifiedTopology
 
   const db = client.db(databaseName)
 
-  // db.collection('users').findOne({_id:new ObjectID('5df50fe63b80ad5d3e6e9d4d')}, (error, user) => {
-  //   if (error) {
-  //     return console.log('Unable to fetch')
-  //   }
-
-  //   console.log(user)
+  // db.collection('users').deleteMany({
+  //   age: 36
+  // }).then((result) => {
+  //   console.log(result)
+  // }).catch((error) => {
+  //   console.log(error)
   // })
 
-  // db.collection('users').find({ age:36 }).toArray((erroe, user) => {
-  //   if (error) {
-  //     return console.log('Unable to fetch')
-  //   }
-
-  //   console.log(user)
+  // db.collection('task').deleteOne({
+  //   description:'Take out the trash'
+  // }).then((result) => {
+  //   console.log(result)
+  // }).catch((error) => {
+  //   console.log(error)
   // })
-
-  db.collection('task').findOne({_id: new ObjectID('5df50455f62bff5cbda0df12')}, (error, task) => {
-    console.log(task)
-  })
-
-  db.collection('task').find({completed: false}).toArray((error, task) => {
-    console.log(task)
-  })
 })
 
+
+
+
+
+
+
+// db.collection('users').updateOne({
+  //   _id: new ObjectID('5df460f176dae75b47a28461')
+  // }, {
+  //   $inc: {
+  //     age: 1
+  //   }
+  // }).then((result) => {
+  //   console.log(result)
+  // }).catch((error) => {
+  //   console.log(error)
+  // })
+
+// db.collection('task').updateMany({
+//   completed: false
+// },{
+//   $set:{
+//     completed: true
+//   }
+// }).then((result) => {
+//   console.log(result.modifiedCount)
+// }).catch((error) => {
+//   console.log(error)
+// })
+
+
+
+
+// db.collection('users').findOne({_id:new ObjectID('5df50fe63b80ad5d3e6e9d4d')}, (error, user) => {
+  //   if (error) {
+    //     return console.log('Unable to fetch')
+    //   }
+    
+    //   console.log(user)
+    // })
+    
+    // db.collection('users').find({ age:36 }).toArray((erroe, user) => {
+      //   if (error) {
+        //     return console.log('Unable to fetch')
+        //   }
+        
+        //   console.log(user)
+        // })
+            
+        // db.collection('task').findOne({_id: new ObjectID('5df50455f62bff5cbda0df12')}, (error, task) => {
+            //   console.log(task)
+            // })
+            
+            // db.collection('task').find({completed: false}).toArray((error, task) => {
+            //   console.log(task)
+            // })
