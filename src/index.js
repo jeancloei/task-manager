36144feec50c3d87ea1ad02e62ba0a7966354b91
Jean Carlos De Castro <jeancarlosdecastro@Jeans-MacBook-Pro.local>
2019-12-15 19:04:36 -0500

    completed update for task

--- src/index.js
+++ src/index.js
@@ -30,17 +30,39 @@ app.get('/users', async (req, res) => {
 
 app.get('/users/:id', async (req, res) => {
   const _id = req.params.id
-
+  
   try {
     const user = await User.findById(_id)
-
+    
     if (!user) {
       return res.status(404).send()
+    }
+    
+    res.send(user)
+  } catch (e) {
+    res.status(500).send()
+  }
+})
+
+app.patch('/users/:id', async (req, res) => {
+  const updates = Object.keys(req.body)
+  const allowedUpdates = ['name', 'email', 'paswword', 'age']
+  const isValidOperation = updates.every((update) => allowedUpdates.includes(update))
+
+  if (!isValidOperation) {
+    return res.status(400).send({ error: 'Invalid update!' })
+  }
+
+  try {
+    const user = await User.findByIdAndUpdate(req.params.id, req.body, { new: true, runValidators: true })
+
+    if (!user) {
+    return res.status(404).send()
     }
 
     res.send(user)
   } catch (e) {
-    res.status(500).send()
+    res.status(400).send()
   }
 })
 
@@ -78,6 +100,27 @@ app.get('/tasks/:id', async (req, res) => {
   } catch (e) {
     res.status(500).send()
   }
+})
+
+app.patch('/tasks/:id', async (req, res) => {
+  const updates = Object.keys(req.body)
+  const allowedUpdates = ['description', 'completed']
+  const isValidOperation = updates.every((update) => allowedUpdates.includes(update))
+
+  if (!isValidOperation) {
+    return res.status(400).send({ error: 'Invalid updates!' })
+  }
+
+  try {
+    const task = await Task.findByIdAndUpdate(req.params.id, req.body, { new: true, runValidators: true })
+    
+    if (!task) {
+      return res.status(404).send(e)
+    }
+
+    res.send(task)
+  } catch (e){}
+    res.status(400).send()
 })
 
 app.listen(port, () => {
