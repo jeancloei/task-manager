8eb2f728617bb6958220cb40c60c6652b75c833a
Jean Carlos De Castro <jeancarlosdecastro@Jeans-MacBook-Pro.local>
2019-12-17 14:08:57 -0500

    refactor CRUD actions using postman

--- src/index.js
+++ src/index.js
@@ -6,6 +6,18 @@ require('./db/mongoose')
 const app = express()
 const port = process.env.PORT || 3000
 
+// app.use((req, res, next) => {
+//   if (req.method === "GET") {
+//     res.send('GET request are disabled')
+//   } else {
+//     next()
+//   }
+// })
+
+// app.use((req, res, next) => {
+//   res.status(503).send("Site is down check back soon!")
+// })
+
 app.use(express.json())
 app.use(userRouter)
 app.use(taskRouter)
@@ -13,15 +25,3 @@ app.use(taskRouter)
 app.listen(port, () => {
   console.log('Server is up on port ' + port)
 })
-
-const jwt = require('jsonwebtoken')
-
-const myFunction = async () => {
-  const token = jwt.sign({ _id: 'abc123' }, 'thisismynewcourese', { expiresIn: '7 days' })
-  console.log(token)
-
-  const data = jwt.verify(token, 'thisismynewcourese')
-console.log(data)
-}
-
-myFunction()
