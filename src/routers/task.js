633442145bd544f4231054e8eabbb547680aa73b
Jean Carlos De Castro <jeancarlosdecastro@Jeans-MacBook-Pro.local>
2019-12-18 13:02:02 -0500

    added timestamps

--- src/routers/task.js
+++ src/routers/task.js
@@ -10,7 +10,7 @@ router.post('/tasks',auth, async (req, res) => {
   })
 
   try {
-    await task.save(task)
+    await task.save()
     res.status(201).send(task)
   } catch (e) {
     res.status(400).send(e)
